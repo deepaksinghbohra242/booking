ea0a4f621a375480f0c63178c88b9e863de4419f
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2023-11-26 11:57:49 +0530

    done

--- client/src/components/menu/Profile.jsx
+++ client/src/components/menu/Profile.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Account from "./Account";
+import Places from "./Places";
+import AddPlaces from "./AddPlaces";
+import Saved from "./Saved";
 
 function Profile() {
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
   function linkClasses(type = null) {
     let classes = "inline-flex gap-2 py-2 px-6 rounded-full";
     if (type === subpage) {
-      classes += " bg-primary text-white ";
+      classes += " bg-red-400 p-2 text-white rounded-2xl text-white ";
     } else {
       classes += " bg-gray-300";
     }
@@ -14,9 +22,99 @@ function Profile() {
   return (
     <>
       <div>
-        <div className="w-full flex justify-center mt-8 gap-8">
-          <Link className={}></Link>
-        </div>
+        <nav className="w-full flex justify-center mt-8 gap-8">
+          <Link className={linkClasses("profile")} to={"/profile"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+            Account
+          </Link>
+          <Link className={linkClasses("places")} to={"/profile/places"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+            MyHosting
+          </Link>
+          <Link className={linkClasses("saved")} to={"/profile/saved"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+              />
+            </svg>
+            Saved
+          </Link>
+          <Link className={linkClasses("addplace")} to={"/profile/addplace"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+              />
+            </svg>
+            Add Place
+          </Link>
+        </nav>
+
+        {
+          subpage === "profile" && <Account />
+        }
+        {
+          subpage === "places" && <Places />
+        }
+        {
+          subpage === "saved" && <Saved />
+        }
+        {
+          subpage === "addplace" && <AddPlaces />
+        }
       </div>
     </>
   );
---
+++ client/src/components/menu/Saved.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RoomCard from '../hotel/RoomCard'
+
+function Saved() {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-2xl mt-5'>saved details</h1>
+      <RoomCard />
+    </div>
+  )
+}
+
+export default Saved
